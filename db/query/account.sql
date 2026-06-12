0e98808d78ec75232e065cb9dfd732cb450a5612
Rajvardhan <rajvardhan.ganji75@gmail.com>
2026-06-12 23:13:12 +0530

    feat: Implement GetAccount and ListAccount queries in account.sql and account.sql.go

--- db/query/account.sql
+++ db/query/account.sql
@@ -7,3 +7,12 @@ INSERT INTO account (
   $1, $2, $3
 ) RETURNING *;
 
+-- name: GetAccount :one
+SELECT * FROM account
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccount :many
+SELECT * FROM account
+ORDER BY id
+LIMIT $1
+OFFSET $2;
